fa2694b79c29a0887e9768f2cf417409c5403d3a
elliscode <45629640+elliscode@users.noreply.github.com>
2024-05-18 01:12:11 -0400

    working snail

--- s3/js/escargo.js
+++ s3/js/escargo.js
@@ -4,26 +4,66 @@ const shareLocation = document.getElementById('share-location');
 
 let locationTimeout = undefined;
 
-function createRandomString(length) {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-  for (let i = 0; i < length; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
+function getSnailLocation(userLocation) {
+  let existingSnail = undefined;
+  try {
+    existingSnail = JSON.parse(localStorage.getItem("escargo-snail-position"));
+  } catch (e) {
+
   }
-  return result;
+  if (!existingSnail && userLocation) {
+    const snailLat = Math.min(90, Math.max(-90, userLocation.lat + (Math.random() - 0.5)));
+    const snailLong = Math.min(180, Math.max(-180, userLocation.long + (Math.random() - 0.5)));
+    existingSnail = {"lat": snailLat, "long": snailLong, "time": Date.now()};
+    localStorage.setItem("escargo-snail-position", JSON.stringify(existingSnail));
+  }
+  return existingSnail;
 }
 
-
-function getUserId() {
-  let existingUser = localStorage.getItem("escargo-user-identifier");
-  if (!existingUser) {
-    existingUser = createRandomString(100);
-    localStorage.setItem("escargo-user-identifier", existingUser);
-  }
-  return existingUser;
+function setSnailLocation(inputSnail) {
+  localStorage.setItem("escargo-snail-position", JSON.stringify(inputSnail));
 }
 
-function getLocation() {
+function setUserLocation(lat, long) {
+  localStorage.setItem("escargo-user-position", JSON.stringify({"lat": lat, "long": long}));
+}
+
+function getUserLocation() {
+  const result = localStorage.getItem("escargo-user-position");
+  if (result) {
+    try {
+      return JSON.parse(result)
+    } catch (e) {
+
+    }
+  }
+  return undefined;
+}
+
+// source https://stackoverflow.com/a/18883819
+const EARTH_RADIUS_FEET = 20902260;
+//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in feet)
+function calcCrowFeet(lat1deg, lon1deg, lat2deg, lon2deg) {
+  let R = EARTH_RADIUS_FEET; // feet
+  let dLat = toRad(lat2deg-lat1deg);
+  let dLon = toRad(lon2deg-lon1deg);
+  let lat1 = toRad(lat1deg);
+  let lat2 = toRad(lat2deg);
+
+  let a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  let d = R * c;
+  return d;
+}
+// Converts numeric degrees to radians
+function toRad(value) {
+    return value * Math.PI / 180;
+}
+
+let userLocationInterval = undefined;
+
+function geolocateUser() {
   shareLocation.style.display = 'none';
   if (navigator.geolocation) {
     title.innerText = "Retrieving your location...";
@@ -31,33 +71,59 @@ function getLocation() {
   } else {
     json.innerHTML = "Geolocation is not supported by this browser.";
   }
+  userLocationInterval = setTimeout(geolocateUser, 9000);
 }
 
-let lat = 0;
-let long = 0;
+const timeToKill = document.getElementById('ttk');
+const SNAIL_SPEED_IN_FEET_PER_SECOND = 0.03281; // ft/s (feet per second)
+const SNAIL_SPEED_IN_FEET_PER_MINUTE = 60 * SNAIL_SPEED_IN_FEET_PER_SECOND; // ft/min (feet per minute)
+
 function tellTheSnailYourPosition(position) {
-  title.innerText = "Notifying the snail...";
-  lat = position.coords.latitude;
-  long = position.coords.longitude;
+  const lat = position.coords.latitude;
+  const long = position.coords.longitude;
+  setUserLocation(lat, long);
   snail.src = 'img/snail-smiling.png';
-
-  const payload = {
-    userId: getUserId(),
-    lat: lat,
-    long: long
-  };
-  const url = API_DOMAIN + "/snail/notify";
-  xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("POST", url, true);
-  xmlHttp.withCredentials = true;
-  xmlHttp.onload = handleSnailResponse;
-  xmlHttp.send(JSON.stringify(payload));
+  displayStatus();
 }
 
-function handleSnailResponse(event) {
-  const result = defaultHandler(event);
-  const distance = result.responseJson.distance;
-  const time = result.responseJson;
+function displayStatus() {
+  const userLocation = getUserLocation();
+  if (!userLocation) {
+    return;
+  }
+  const snailLocation = getSnailLocation(userLocation);
+  if (!snailLocation) {
+    return;
+  }
+  const distanceInFeet = Math.max(0, Math.abs(calcCrowFeet(userLocation.lat,userLocation.long,snailLocation.lat,snailLocation.long)) - 30);
+  title.innerText = `The snail is ${displayDistance(distanceInFeet)} away`;
+  let timeRemaining = distanceInFeet / SNAIL_SPEED_IN_FEET_PER_MINUTE; // minutes
+  timeToKill.parentElement.style.display = 'block';
+  timeToKill.innerText = displayTime(timeRemaining);
+  shareLocation.innerText = 'Allow your current location';
+  if (shareLocation.style.display == 'none') {
+    directionsButton.style.display = 'block';
+  }
+}
+
+function displayTime(timeInMinutes) {
+  let timeInSeconds = timeInMinutes * 60;
+  if (timeInSeconds < 60) {
+    return `${Math.round(timeInSeconds * 10) / 10} seconds`;
+  } else if (timeInSeconds < 3600) {
+    return `${Math.round(timeInSeconds / 60 * 10) / 10} minutes`;
+  } else if (timeInSeconds < 86400) {
+    return `${Math.round(timeInSeconds / 360 * 10) / 10} hours`;
+  } 
+  return `${Math.round(timeInSeconds / 86400 * 10) / 10} days`;
+}
+
+function displayDistance(distanceInFeet) {
+  let outputNumber = Math.round(distanceInFeet)
+  if (outputNumber < 5250) {
+    return `${Math.round(outputNumber)} feet`;
+  }
+  return `${Math.round(outputNumber/5250)} miles`;
 }
 
 function defaultHandler(event) {
@@ -77,3 +143,64 @@ function defaultHandler(event) {
   }
   return {statusCode: xmlHttp.status, responseJson: result};
 }
+
+const funFact = document.getElementById('fun-fact');
+
+function showRandomSnailFact() {
+  funFact.innerText = funFacts[Math.floor(Math.random() * funFacts.length)];
+}
+
+let snailFactInterval = setInterval(showRandomSnailFact, 10000);
+showRandomSnailFact();
+
+const directionsButton = document.getElementById('get-directions');
+
+function moveSnail() {
+  const userLocation = getUserLocation();
+  if (!userLocation) {
+    return;
+  }
+  const snailLocation = getSnailLocation();
+  if (!snailLocation) {
+    return;
+  }
+  let timeDiff = Date.now() - snailLocation.time;
+  let distanceTraveled = SNAIL_SPEED_IN_FEET_PER_SECOND * (timeDiff / 1000);
+  let latDiff = userLocation.lat - snailLocation.lat;
+  let lonDiff = userLocation.long - snailLocation.long;
+  let latNorm = latDiff / (latDiff + lonDiff);
+  let lonNorm = lonDiff / (latDiff + lonDiff)
+  let lonComponent = distanceTraveled * lonNorm;
+  let latComponent = distanceTraveled * latNorm;
+  let latPart = (latComponent / EARTH_RADIUS_FEET) * (180 / Math.PI);
+  let lonPart = (lonComponent / EARTH_RADIUS_FEET) * (180 / Math.PI) / Math.cos(snailLocation.lat * Math.PI/180);
+  if (userLocation.lat > snailLocation.lat) {
+    snailLocation.lat = snailLocation.lat + latPart;
+  } else {
+    snailLocation.lat = snailLocation.lat - latPart;
+  }
+
+  if (userLocation.long > snailLocation.long) {
+    snailLocation.long = snailLocation.long + lonPart;
+  } else {
+    snailLocation.long = snailLocation.long - lonPart;
+  }
+  snailLocation.time = Date.now();
+
+  setSnailLocation(snailLocation);
+  snail.classList.add('moving');
+  displayStatus();
+}
+
+function getDirectionsToSnail() {
+  const snailLocation = getSnailLocation();
+  let coordsString = `${snailLocation.lat},${snailLocation.long}`;
+  let googleUrl = "https://www.google.com/maps/search/?api=1&query=" + coordsString;
+  let openLink = document.createElement('a');
+  openLink.href=googleUrl;
+  openLink.target="_blank";
+  openLink.click();
+}
+
+displayStatus();
+let snailMoveInterval = setInterval(moveSnail, 1000);
---
+++ s3/js/fun-facts.js
@@ -0,0 +1,41 @@
+// source https://facts.net/snail-facts/
+// source https://www.exceptionalsmiles.dental/blog/10-interesting-weird-fun-facts-animals-teeth-exceptional-smiles-family-dentistry/
+// source https://www.reddit.com/r/autism/comments/w20h8s/give_me_all_the_snail_facts_ya_got/
+// source https://www.birdsandblooms.com/gardening/backyard-wildlife/snail-facts/
+// source http://www.in.gov/dnr/kids/5878.htm
+// source http://www.ucmp.berkeley.edu/taxa/inverts/mollusca/mollusca.php
+// source https://snail-world.com/snail-facts/
+// source https://www.wolframalpha.com/input?i=%282.5cm%2Fs+to+mph%29*24hour
+// source https://www.wolframalpha.com/input?i=1mile%2F%281cm%2Fs+to+mph%29
+const funFacts = [
+    'real life snails sleep for up to three years, but this snail never sleeps',
+    'snails have the most teeth of any animal, and this snail has even more',
+    'a snail mouth has no jaw, and this snail can swallow you whole',
+    'snails first evolved 500 million years ago',
+    'there are over 60,000 species of snails in the world',
+    'freshwater snails usually reproduce asexually',
+    'snail mucin is used in cosmetics for making skin clearer and softer',
+    'snail food needs to be rich in protein for them to grow, and you are rich in protein',
+    'the oldest fully preserved snail dates back to 99 million years ago',
+    'snails are born with their shells, and their shells grow with them',
+    'unlike hermit crab shells, snail shells cannot be replaced',
+    'the fastest snail moves at 2.5cm/s, or 1.34 miles per day',
+    'the smallest snail on earth is native to China',
+    'crushed snail shells are used in manufacturing and cosmetics',
+    'snails are not insects, they are mollusks',
+    'snails actually walk using a single foot',
+    'the venom from the cone snail can kill a human, however this snail will kill you with pure hatred',
+    'the giant whelk is the worlds largest snail',
+    'the average snail moves at 1cm/s, it takes over 44 hours to go a mile',
+    'around 1/3 of snails die after giving birth',
+    'snails used to be a staple food during times of famine',
+    'salt will kill a snail, but not this snail, he is immortal',
+    'snail eggs are considered a delicacy, and are known as "white caviar"',
+    'the teeth of an aquatic snail called the limpet are the strongest known biological material on Earth, even stronger than titanium',
+    'snails can feel what\'s on their shell',
+    'most snail species are hermaphrodites, so they have both male and female reproductive organs',
+    'calcium carbonate is the main component of the snail shells',
+    'the size of the shell of a snail reflects its age',
+    'snails cannot hear, as they do not have ears or an ear canal',
+    'snails are strong and can lift up to 10 times their body weight in a vertical position'
+];
